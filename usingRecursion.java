0a38f13bbf884867ce3c57fdf2e1a58308f96897
pranjalicapg <157630794+pranjalicapg@users.noreply.github.com>
2024-01-25 11:20:59 +0530

    Update usingRecursion.java

---
+++ usingRecursion.java
@@ -0,0 +1,21 @@
+package usingrecursion;
+
+public class usingRecursion
+ {
+   public static void main (String[]args)
+   {
+
+     int num = 12345;
+       System.out.println ("Sum of digits : " + getSum (num));
+   }
+
+   static int getSum (int num)
+   {
+
+     if (num == 0)
+       return 0;
+
+     return (num % 10) + getSum (num / 10);
+   }
+
+ }
